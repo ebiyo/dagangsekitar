aecd94e6949db48b9321f0c3bb05e00176f8a0fb
ebiyo <ariefridzki@gmail.com>
2024-09-19 21:09:37 +0700

    commit for Tugas 4

--- dagangsekitar/settings.py
+++ dagangsekitar/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r$&d4d+7z@cwub9v(*^(+#_o7a*l=@dxgq(c48=g6do*s_)yz!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "arief-ridzki-dagangsekitar.pbp.cs.ui.ac.id"]
 
